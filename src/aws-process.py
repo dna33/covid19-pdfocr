f90dd33ca28c6c9faabd133150ca2f58c259bff6
sebastian cornejo <sebastian.cornejo@gmail.com>
2020-04-20 14:25:05 -0400

    got tables to csv from reporte 2020-04-01

--- src/aws-process.py
+++ src/aws-process.py
@@ -2,6 +2,7 @@ import time
 import boto3
 import glob
 from botocore.exceptions import ClientError
+from pprint import pprint
 
 #PDF are processed asynchronously: files must be on S3
 
@@ -125,20 +126,96 @@ def getJobResults(jobId):
         if ('NextToken' in response):
             nextToken = response['NextToken']
 
-        print(type(pages))
-        for page in pages:
-            print(type(page))
-            print('page is :\n' + str(page))
-            blocks = page['Blocks']
-            print('blocks are :\n' + str(blocks))
-            print(type(blocks))
-            blocks_map = {}
-            table_blocks = []
-            for block in blocks:
-                blocks_map[block['Id']] = block
-                if block['BlockType'] == "TABLE":
-                   print(block)
+        print('pages is type: ' + str(type(pages)))
+        # for page in pages:
+        #     print(type(page))
+        #     #pprint(page)
+        #     blocks = page['Blocks']
+        #     print('blocks are:')
+        #     print(type(blocks))
+        #     blocks_map = {}
+        #     table_blocks = []
+        #     for block in blocks:
+        #         blocks_map[block['Id']] = block
+        #         if block['BlockType'] == "TABLE":
+        #            print(block)
 
+    return pages
+
+def get_table_csv_results(pages):
+    # Get the text blocks
+    blocks_map = {}
+    table_blocks = []
+    blocks_list = []
+    for page in pages:
+        blocks_list.append(page['Blocks'])
+        print(type(blocks_list))
+
+    for blocks in blocks_list:
+
+        for block in blocks:
+            blocks_map[block['Id']] = block
+            if (block['BlockType']) == "TABLE":
+                table_blocks.append(block)
+
+        if len(table_blocks) <= 0:
+            return "<b> NO Table FOUND </b>"
+
+    csv = ''
+    for index, table in enumerate(table_blocks):
+        csv += generate_table_csv(table, blocks_map, index +1)
+        csv += '\n\n'
+
+    return csv
+
+
+def generate_table_csv(table_result, blocks_map, table_index):
+    rows = get_rows_columns_map(table_result, blocks_map)
+
+    table_id = 'Table_' + str(table_index)
+
+    # get cells.
+    csv = 'Table: {0}\n\n'.format(table_id)
+
+    for row_index, cols in rows.items():
+
+        for col_index, text in cols.items():
+            csv += '{}'.format(text) + ","
+        csv += '\n'
+
+    csv += '\n\n\n'
+    return csv
+
+
+def get_rows_columns_map(table_result, blocks_map):
+    rows = {}
+    for relationship in table_result['Relationships']:
+        if relationship['Type'] == 'CHILD':
+            for child_id in relationship['Ids']:
+                cell = blocks_map[child_id]
+                if cell['BlockType'] == 'CELL':
+                    row_index = cell['RowIndex']
+                    col_index = cell['ColumnIndex']
+                    if row_index not in rows:
+                        # create new row
+                        rows[row_index] = {}
+                    # get the text value
+                    rows[row_index][col_index] = get_text(cell, blocks_map)
+    return rows
+
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] =='SELECTED':
+                            text += 'X '
+    return text
 
 
 if __name__ == "__main__":
@@ -162,3 +239,5 @@ if __name__ == "__main__":
     print("Started job with id: {}".format(jobId))
     if (isJobComplete(jobId)):
         response = getJobResults(jobId)
+        result = get_table_csv_results(response)
+        print(result)
