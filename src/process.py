f527f96abe867686f5ac6356d4eedffdfb133fca
sebastian cornejo <sebastian.cornejo@gmail.com>
2020-04-23 15:54:05 -0400

    testing s3

--- src/process.py
+++ src/process.py
@@ -4,37 +4,46 @@ from postAwsProcessing import *
 from os import listdir
 
 if __name__ == '__main__':
-    test = False
+    test = True
     if test:
-        jobId = '3256e214371aea597f64a056b4094fef1e612d5accb2bffdd19f3157eb2c11f8'
-        print("Started job with id: {}".format(jobId))
-        if (isJobComplete(jobId)):
-            response = getJobResults(jobId)
-            # result = get_table_csv_results(response)
-            result = get_table_pd_results(response)
-            a = pandizer(result)
-            print(a)
+        # jobId = '3256e214371aea597f64a056b4094fef1e612d5accb2bffdd19f3157eb2c11f8'
+        # print("Started job with id: {}".format(jobId))
+        # if (isJobComplete(jobId)):
+        #     response = getJobResults(jobId)
+        #     # result = get_table_csv_results(response)
+        #     result = get_table_pd_results(response)
+        #     a = pandizer(result)
+        #     print(a)
 
+        myS3 = 'do-covid-19'
+        reportePath = '../input/ReporteDiario/*.pdf'
+        rep = preparePathsForUpload(reportePath)
+        for eachrep in rep:
+            if not checkIfFileIsOnS3(myS3, eachrep[1]):
+                upload_file(eachrep[0], myS3, eachrep[1])
 
     else:
+        myS3 = 'do-covid-19'
+
         # REPORTE DIARIO
         obtenerReporteDiario('https://www.gob.cl/coronavirus/cifrasoficiales/', '../input/ReporteDiario/')
-        myS3 = 'do-covid19'
         reportePath = '../input/ReporteDiario/*.pdf'
-
         rep = preparePathsForUpload(reportePath)
         # Como sabemos si un archivo ya se proceso??
         # Revisemos el output
         outputFiles = listdir('../output/raw/ReporteDiario')
 
         for eachrep in rep:
+            if not checkIfFileIsOnS3(myS3, eachrep[1]):
+                upload_file(eachrep[0], myS3, eachrep[1])
+
             sourceFile = eachrep[1].split('/')[1].replace('.pdf', '')
 
             if [x for x in outputFiles if sourceFile in x]:
                 print(sourceFile + ' was already processed')
             else:
                 print('processing ' + sourceFile)
-                upload_file(eachrep[0], 'do-covid19', eachrep[1])
+                upload_file(eachrep[0], myS3, eachrep[1])
                 jobId = startJob(myS3, eachrep[1])
                 myfile = open("jobs.log", 'a+')
                 myfile.write(eachrep[1] + ': ' + jobId + '\n')
@@ -43,7 +52,7 @@ if __name__ == '__main__':
                     response = getJobResults(jobId)
                     result = get_table_pd_results(response)
                     a = pandizer(result)
-                    dumpDict2csv(a, sourceFile,'../output/raw/ReporteDiario/')
+                    dumpDict2csv(a, sourceFile, '../output/raw/ReporteDiario/')
 
         # INFORME SITUACION
         obtenerSituacionCOVID19('http://epi.minsal.cl/informes-covid-19/', '../input/InformeSituacionCOVID19/')
@@ -57,7 +66,7 @@ if __name__ == '__main__':
                 print(sourceFile + ' was already processed')
             else:
                 print('processing ' + sourceFile)
-                upload_file(eachsit[0], 'do-covid19', eachsit[1])
+                upload_file(eachsit[0], myS3, eachsit[1])
                 jobId = startJob(myS3, eachsit[1])
                 myfile = open("jobs.log", 'a+')
                 myfile.write(eachsit[1] + ': ' + jobId + '\n')
@@ -81,7 +90,7 @@ if __name__ == '__main__':
                 print(sourceFile + ' was already processed')
             else:
                 print('processing ' + sourceFile)
-                upload_file(eachinf[0], 'do-covid19', eachinf[1])
+                upload_file(eachinf[0], myS3, eachinf[1])
                 jobId = startJob(myS3, eachinf[1])
                 myfile = open("jobs.log", 'a+')
                 myfile.write(eachinf[1] + ': ' + jobId + '\n')
